5cffcf782fd2438071201bf45a290763e2d10e57
Janella Navarro <navarro.jdp@gmail.com>
2022-09-30 23:00:06 +0800

    linked nav bar

--- capswebsite/urls.py
+++ capswebsite/urls.py
@@ -16,5 +16,3 @@ urlpatterns = [
     path('survey_question',views.survey_question, name="survey_question"),
     path('student_navbar',views.student_navbar, name="student-navbar")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
