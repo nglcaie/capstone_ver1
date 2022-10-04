68d39a41e735e9de8eee3356beecc9f0ac2a2131
Sofia Javier <javiersofia.as@gmail.com>
2022-10-04 21:01:53 +0800

    conditional login

--- capswebsite/admin.py
+++ capswebsite/admin.py
@@ -2,27 +2,27 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
 from .models import *
-
-
+ 
+ 
 # Register your models here.
 class UserAdmin(UserAdmin):
-    list_display = ('pk','email','date_of_inactive','is_active','is_admin','is_student','date_joined','last_login')
-    search_fields = ('email',)
+    list_display = ('pk','email','numberID','date_of_inactive','is_active','is_admin','is_student','date_joined','last_login')
+    search_fields = ('email','numberID',)
     readonly_fields = ('date_joined','last_login')
     ordering = ('pk',)
     filter_horizontal = ()
     list_filter = ('is_active','is_student','is_admin')
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password','numberID')}),
         ('Permissions', {'fields': ('is_active','date_of_inactive','is_admin','is_student')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2','is_admin','is_student'),
+            'fields': ('email','numberID', 'password1', 'password2','is_admin','is_student'),
         }),
     )
-
-
+ 
+ 
 admin.site.register(User,UserAdmin)
 admin.site.unregister(Group)
--- capswebsite/models.py
+++ capswebsite/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.validators import RegexValidator
@@ -5,22 +6,22 @@ from django.db.models.deletion import CASCADE, DO_NOTHING
 from datetime import datetime
 from django.utils import timezone
 import os, random
-from django.utils.html import mark_safe 
-
+from django.utils.html import mark_safe
+ 
 # Create your models here.
-
+ 
 class UserManager(BaseUserManager):
     def create_user(self,email, password=None):
         if not email:
             raise ValueError("Users must have an email address")
-
+ 
         user = self.model(
             email=self.normalize_email(email),
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+ 
     def create_superuser(self,email,password):
         """
         Creates and saves a superuser with the giv email, name and password.
@@ -33,32 +34,44 @@ class UserManager(BaseUserManager):
         user.is_admin = True
         user.save(using=self._db)
         return user
-        
+       
 class User(AbstractBaseUser):
-    email = models.EmailField(verbose_name="Email",max_length=60,unique=True)
+    email_error_message = 'Email must be: @plm.edu.ph'
+    email_regex = RegexValidator(
+    regex=r'^[A-Za-z0-9._%+-]+@plm.edu.ph$',
+    message=email_error_message
+    )
+    # ID number code. Can be copy pasted to suit ID code for certain user.
+    ID_error_message = 'Faculty ID must be entered in format: 20XXXXXXX'
+    ID_regex = RegexValidator(
+        regex=r'^20\d{7}$',
+        message=ID_error_message
+    )
+    email = models.EmailField(verbose_name="Email",max_length=60,unique=True,validators=[email_regex])
+    numberID = models.CharField(validators=[ID_regex], max_length=50,unique=True, verbose_name='Number ID', null=True)
     is_active = models.BooleanField(default=True)
     date_of_inactive = models.DateTimeField(verbose_name="Date of Inacitve",null=True,blank=True)
     is_student = models.BooleanField(default=False, verbose_name='Student')
-    is_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False,verbose_name='Admin')
     date_joined = models.DateTimeField(verbose_name='Date Joined',auto_now_add=True)
     last_login = models.DateTimeField(verbose_name="Last Login",null=True,blank=True)
-
+ 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
+ 
     objects = UserManager()
-
+ 
     def __str__(self):
         return f"{self.email}"
-
+ 
     def has_perm(self, perm, obj=None):
         return True
-
+ 
     def has_module_perms(self, app_label):
         """Does the user have permissions to view the app `app_label`?"""
         # Simplest possible answer: Yes, always
         return True
-
+ 
     @property
     def is_staff(self):
         """Is the user a member of staff?"""
